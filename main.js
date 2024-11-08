c9445cdb50a7ba16984528fed1ea4b739dcb64f3
Myra <0.maysin.0@gmail.com>
2024-11-07 20:42:30 -0800

    Adds it115 site along with js file and script to reference for cleanliness

---
+++ main.js
@@ -0,0 +1,27 @@
+function elementToggle() {
+    var x = document.getElementById("games");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+//https://tinyurl.com/dynamic-html-css-checkers
+document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+
+document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+    
+    //set current year in span with id of this-year
+    let d = new Date(); let thisYear = d.getFullYear();
+    document.getElementById("this-year").innerHTML = thisYear;
+
+//manages mobile nav 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}   
